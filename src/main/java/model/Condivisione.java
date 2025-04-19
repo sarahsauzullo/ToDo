eacf20cd0e1d566135f3eb1343bd062b18bdfcb3
TurboNafta <luiginappo18@gmail.com>
2025-04-19 17:26:51 +0200

    Modifica tipo partecipante
    Co-Authored-By: sarahsauzullo <206846344+sarahsauzullo@users.noreply.github.com>
    Co-Authored-By: GiuliaVajro <207347426+GiuliaVajro@users.noreply.github.com>

--- src/main/java/model/Condivisione.java
+++ src/main/java/model/Condivisione.java
@@ -1,8 +1,10 @@
 package model;
 
+import java.util.ArrayList;
+
 public class Condivisione {
     private String autore;
-    private String partecipanti;
+    private ArrayList<Utente> partecipanti;
 
     //gestisco la relazione con utente
     private Utente utente;
